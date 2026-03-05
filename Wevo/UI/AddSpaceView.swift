e3108fe299dfcdcc75675a8b176c078ea5c14e2a
Hidemune Takahashi <h1d3mun3@gmail.com>
2026-03-05 22:29:38 +0900

    AddSpaceViewへのRoutingを追加

---
+++ Wevo/UI/AddSpaceView.swift
@@ -0,0 +1,18 @@
+//
+//  AddSpaceView.swift
+//  Wevo
+//
+//  Created by hidemune on 3/5/26.
+//
+
+import SwiftUI
+
+struct AddSpaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddSpaceView()
+}
