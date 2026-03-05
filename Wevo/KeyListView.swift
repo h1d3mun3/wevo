34b78fe3335844177b553c6d0b06cf653250f215
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2026-03-05 20:53:58 +0900

    Key管理をするためのKeyListViewを追加

---
+++ Wevo/KeyListView.swift
@@ -0,0 +1,18 @@
+//
+//  IdentityKeyListView.swift
+//  Wevo
+//
+//  Created by hidemune on 3/5/26.
+//
+
+import SwiftUI
+
+struct IdentityKeyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IdentityKeyListView()
+}
