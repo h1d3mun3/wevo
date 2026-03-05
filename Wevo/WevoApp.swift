8b2321a9ee8db31d4414bd30138b0c6401f33aa4
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2026-03-05 22:20:00 +0900

    SpaceSwiftDataの永続化宣言を追加

--- Wevo/WevoApp.swift
+++ Wevo/WevoApp.swift
@@ -13,6 +13,7 @@ struct WevoApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
             Item.self,
+            SpaceSwiftData.self
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
