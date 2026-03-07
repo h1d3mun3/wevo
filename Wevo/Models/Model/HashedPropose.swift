5635f1cd93b2966717b7b84b7bcf8a53f4376c37
Hidemune Takahashi <h1d3mun3@gmail.com>
2026-03-07 17:18:14 +0900

    HashedProposeのイニシャライザからデフォルト引数を削除。

--- Wevo/Models/Model/HashedPropose.swift
+++ Wevo/Models/Model/HashedPropose.swift
@@ -13,20 +13,13 @@ struct HashedPropose: Codable, Identifiable {
     let id: UUID
     let payloadHash: String
     let signatures: [Signature]
-    let createdAt: Date?
-
-    enum CodingKeys: String, CodingKey {
-        case id
-        case payloadHash = "payloadHash"
-        case signatures
-        case createdAt = "createdAt"
-    }
+    let createdAt: Date
     
     init(
         id: UUID,
         payloadHash: String,
         signatures: [Signature],
-        createdAt: Date? = Date()
+        createdAt: Date
     ) {
         self.id = id
         self.payloadHash = payloadHash
